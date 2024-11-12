18c28d0fbdda3ce49f2c1c4f64ee631b20bb6ddc
Nadezhda490 <lebedinskaya200303@gmail.com>
2024-11-12 18:00:30 +0300

    биография

---
+++ sout/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Меня зовут Надежда и мне 21 год.");
+        System.out.println("Я живу в городе Йошкар-Ола и учусь в университете на экономическом факультете.");
+        System.out.println("Я выбрала программирование, потому что меня интересует данная тема.");
+        System.out.println("В будущем я хочу стать успешным программистом.");
+    }
+}
